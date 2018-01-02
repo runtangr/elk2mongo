a9c8517b1dd61e4c270647633e119e02d65a0e5d
tangrui <1048393815@qq.com>
2018-01-02 10:53:12 +0800

    remove

--- src/main/python/core/core.py
+++
@@ -1,6 +0,0 @@
-import redis
-from mongoengine import connect
-# init mongodb and redis
-connect('test_database')
-pool = redis.ConnectionPool(host='localhost', port=6379)
-r = redis.StrictRedis(connection_pool=pool)
