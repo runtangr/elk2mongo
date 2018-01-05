92a3fa036ed8f428b54965acc715d23ac3e22ac1
tangrui <1048393815@qq.com>
2018-01-05 08:45:03 +0800

    rm

--- src/main/python/sync/core/core.py
+++
@@ -1,9 +0,0 @@
-import redis
-from mongoengine import connect
-# init mongodb and redis
-connect('test_database')
-pool_user = redis.ConnectionPool(host='localhost', port=6379, db=0)
-r_user = redis.StrictRedis(connection_pool=pool_user)
-
-pool_sima = redis.ConnectionPool(host='localhost', port=6379, db=1)
-r_sima = redis.StrictRedis(connection_pool=pool_sima)
